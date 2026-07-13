2778985dbadf9fef2dc9f4e60a9335971650c5fd
RJ <riley.bell6@education.nsw.gov.au>
2026-07-14 00:47:11 +1000

    edit

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
 app = Flask(__name__)
+app.secret_key = "supersecretkey"
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Year-12-Assessment-2.db"  
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  
